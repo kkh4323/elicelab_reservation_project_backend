8d40dd44bda9987c97d1c600250417596e929395
Kangho <kkh4323@naver.com>
2024-10-10 19:30:28 +0900

    Add new config path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AppConfigModule } from './config/config.module';
-import { DatabaseModule } from './database/database.module';
+import { AppController } from '@root/app.controller';
+import { AppService } from '@root/app.service';
+import { AppConfigModule } from '@config/config.module';
+import { DatabaseModule } from '@database/database.module';
 
 @Module({
   imports: [AppConfigModule, DatabaseModule],
